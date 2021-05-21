147ab7db0000eebd6efc423ba74242847283f25c
Diego-DevsS <diegocaetano444@outlook.com>
2021-05-21 03:48:20 -0300

    Add styles topbar

--- src/components/topBar/topbar.jsx
+++ src/components/topBar/topbar.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
-// import { FaPlusCircle } from 'react-icons/fa';
+import { FaPlusCircle, } from 'react-icons/fa';
+import { GiSecretBook } from 'react-icons/gi';
+
+import "./topbar.scss"
+// if(document.querySelector("textarea").ra=('hidden')) {
+//   alert("OK");
+// }
 
 const TopBar = () => {
-    return (
-      <nav className="topBar">
-        <input type="text" />
-        <button>Enter</button>
-        {/* <FaPlusCircle /> */}
-      </nav>
-    )
+  return (
+    <nav className="topBar">
+      <div className="edgesEfect"/>
+      <h1 className="logoIcon"><GiSecretBook /></h1>
+      <div className="text">
+        <input type="text"/>
+        <textarea className="textArea" type="text" hidden/>
+      </div>
+        <button className="btn-addPlus"><FaPlusCircle /></button>
+    </nav>
+  )
 }
 
 export default TopBar;
