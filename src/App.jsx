028fe4495a039aed49325ec00d38b2a92ed1427e
Diego-DevsS <diegocaetano444@outlook.com>
2021-05-21 21:29:34 -0300

    Update code

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,9 @@ import Notes from "./components/notes/notes";
 import "./App.scss";
 
 const App = () => {
-  let [notes, setNotes] = useState([
-    {
-      id: 1,
-      title: "Aprender HTML",
-      contentText: "LoremEsto aprendendo React a 3dias, ja sabia um poucoLorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quas earum provident atque est dicta. Excepturi, quibusdam minus? Aliquid corrupti saepe ullam ipsam officia eaque perferendis sequi dolore amet similique? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quas earum provident atque est dicta. Excepturi, quibusdam minus? Aliquid corrupti saepe ullam ipsam officia eaque perferendis sequi dolore amet similique?"
-    },
-    {
-      id: 2,
-      title: "Aprender CSS",
-      contentText: "LoremEsto aprendendo React a 3dias, ja sabia um poucoLorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quas earum provident atque est dicta. Excepturi, quibusdam minus? Aliquid corrupti saepe ullam ipsam officia eaque perferendis sequi dolore amet similique? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quas earum provident atque est dicta. Excepturi, quibusdam minus? Aliquid corrupti saepe ullam ipsam officia eaque perferendis sequi dolore amet similique?"
-    },
-    {
-      id: 3,
-      title: "Aprender javaScript",
-      contentText: "LoremEsto aprendendo React a 3dias, ja sabia um poucoLorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quas earum provident atque est dicta. Excepturi, quibusdam minus? Aliquid corrupti saepe ullam ipsam officia eaque perferendis sequi dolore amet similique? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quas earum provident atque est dicta. Excepturi, quibusdam minus? Aliquid corrupti saepe ullam ipsam officia eaque perferendis sequi dolore amet similique?"
-    },
-    {
-      title: "Aprender React",
-      
-    }
-    
-  ]);
-  const handleNotionAddition = (notionTitle) => {
-    const newNote = [...notes, {
-      title: notionTitle,
-      contentText: "LoremEsto aprendendo React a 3dias, ja sabia um poucoLorem ipsum",
-      id: Math.random(10),
-    }];
 
-    setNotes(newNote);
-  }
+  let [notes, setNotes] = useState([]);
+
 	return (
 		<>
 			<div className="container">
--- src/components/topBar/topbar.jsx
+++ src/components/topBar/topbar.jsx
@@ -5,21 +5,44 @@ import { GiSecretBook } from 'react-icons/gi';
 
 import "./topbar.scss";
 
-const TopBar = ( {handleNotionAddition} ) => {
+const TopBar = () => {
   const [inputTitle, setInputTitle] = useState('');
   const [inputText, setInputText] = useState('');
 
-  const handleTitleEntryChange = (e) => {
-    setInputTitle(e.target.value);
+  // const handleTitleEntryChange = (e) => {
+  //   setInputTitle(e.target.value);
 
-  }
-  const handleTextEntryChange = (e) =>  {
-    setInputText(e.target.value);
+  // }
+  // const handleTextEntryChange = (e) =>  {
+  //   setInputText(e.target.value);
+  // }
+
+  // useEffect(() => {
+  //   const data = localStorage.getItem('NotePad@notes')
+  //   const setInputText = data ? JSON.parse(data) : []
+
+  //   setInputText(notes)
+
+  // }, [])
+
+  const handleSubmit = () => {
+    if(!inputTitle || inputTitle === '') return console.log('error title null')
+    const data = localStorage.getItem('NotePad@notes')
+    const oldData = data ? JSON.parse(data) : []
+    
+    const note = {
+      inputTitle,
+      inputText
+    }
+
+    const newData = [
+      note,
+      ...oldData
+    ]
+
+    localStorage.setItem('NotePad@notes', JSON.stringify(newData))
   }
 
-  const handleNotionClick = () => {
-    console.log(handleNotionAddition(inputTitle));
-  }
 
   return (
     <nav className="topBar">
@@ -27,21 +50,26 @@ const TopBar = ( {handleNotionAddition} ) => {
       <h1 className="logoIcon"><GiSecretBook /></h1>
       <div className="text">
         <input
-          onChange={handleTitleEntryChange}
+          onChange={e => setInputTitle(e.target.value)}
           type="text"
           placeholder="Título"
           value={inputTitle}
         />
         <textarea
-        onChange={handleTextEntryChange}
+        onChange={e => setInputText(e.target.value)}
         value={inputText}
         className="textArea"
         placeholder="texto"
         type="text"/>
       </div>
-      <button className="btn-addPlus" onClick={handleNotionClick}>
-        <FaPlusCircle />
+
+      <button
+        className="btn-addPlus"
+        onClick={handleSubmit}
+        >
+          <FaPlusCircle/>
       </button>
+      {inputText && inputText[0].inputTitle}
     </nav>
   )
 }
