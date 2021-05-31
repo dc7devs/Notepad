a9d80c1acabe0ee0369bb67a97e00d1b0e70b7d8
Miguel-Coruj <miguelcoruj@gmail.com>
2021-05-31 17:46:00 -0300

    📦 Button Mute

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,55 @@
 import React, { useEffect, useState } from "react"
 
+import { FaVolumeMute, FaVolumeUp } from "react-icons/fa"
 import TopBar from "./components/topBar/topbar"
 import Notes from "./components/notes/notes"
 import Footer from "./components/footer/footer"
+import notesController from "./controllers/notes.controller"
 
 import "./App.scss"
 import sample from "./assets/video.mp4"
 
 const App = () => {
 	// notes, matriz usada para inserção das notas(JSON)
-	const [notes, setNotes] = useState([]);
-
-	// Data e Hora que a nota foi criado
-	let currentTime = new  Date()
-	let currentTimeHours = currentTime.getHours()
-	let currentTimeMinutes = currentTime.getMinutes()
-	let currentTimeDate = currentTime.getDate()
-	let currentTimeMonth = currentTime.getMonth() +1
-	if (currentTimeMonth < 10) {
-		currentTimeMonth = `0${currentTimeMonth}`
-	}
-	let currentTimeYear = currentTime.getFullYear()
-	let timeFormatted = `${currentTimeDate}/${currentTimeMonth}/${currentTimeYear} ás ${currentTimeHours}:${currentTimeMinutes}`;
-	const [time, setTime] = useState(timeFormatted);
-
-	function CreateData(note) {
-		const data = localStorage.getItem('NotePad@notes');
-		const oldData = data ? JSON.parse(data) : [];
-
-		// note recebe id
-		if(notes.length > 0)
-			note.id = (notes[notes.length -1].id +1)
-		else
-			note.id = 0
-		
-		setTime(timeFormatted)
-		note.lastEditionTime = time
-
-		const newData = [
-			...oldData,
-			note,
-		]
-
-		localStorage.setItem('NotePad@notes', JSON.stringify(newData));
-
-		setNotes(newData);
-	}
-	function RemoveData(content) {
-		let storedContent = JSON.parse(localStorage.getItem("NotePad@notes"));
-		storedContent.splice(content.id, 1)
-		setNotes(storedContent);
-		
-		// Atualização dos IDs restantes, apos a remoção
-		let idOperation = storedContent.slice(content.id, storedContent.length +1)
-		for (let i in idOperation) {
-			idOperation[i].id = idOperation[i].id -1;
-		}
-
-		localStorage.setItem('NotePad@notes', JSON.stringify(storedContent));
-	}
+	const [notes, setNotes] = useState([])
+	const [isUpNotes, setIsUpNotes] = useState(false)
+	const [isMute, setIsMute] = useState(false)
 
 	useEffect(() => {
-		const data = localStorage.getItem('NotePad@notes');
-		const notes = data ? JSON.parse(data) : [];
+		setNotes(notesController.find())
 
-		setNotes(notes);
-	}, []);
+		setIsUpNotes(false)
+	}, [isUpNotes])
+
+	function handleCreateNote(newNote) {
+		notesController.create(newNote)
+		
+		setIsUpNotes(true)
+	}
+
+	function handleRemoveNote(removeNote) {
+		notesController.remove(removeNote)
+
+		setIsUpNotes(true)
+	}
 
 	return (
 		<>
+			<button 
+				className="mute"
+				onClick={() => setIsMute(!isMute)}
+			>
+				{isMute? 
+					<FaVolumeMute/>
+					:
+					<FaVolumeUp/>
+				}
+			</button>
 			<div className="container">
-				<TopBar
-					CreateData={CreateData}
-				/>
-				<Notes
-					notes={notes}
-					RemoveData={RemoveData}
-				/>
+				<TopBar CreateData={handleCreateNote} />
+				<Notes notes={notes} RemoveData={handleRemoveNote} />
 				<Footer />
-				<video className='myVideo' autoPlay="true" muted loop>
+				<video className='myVideo' autoPlay={true} muted={isMute} loop={true}>
 					<source src={sample} type='video/mp4'/>
 				</video>
 			</div>
