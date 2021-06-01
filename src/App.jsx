631f68935b8c03548ac13264c89e2b963951dc69
Diego-DevsS <diegocaetano444@outloo.com>
2021-05-31 22:38:04 -0300

    code refactoring and bug updatin

--- src/App.jsx
+++ src/App.jsx
@@ -3,83 +3,67 @@ import React, { useEffect, useState } from "react"
 import TopBar from "./components/topBar/topbar"
 import Notes from "./components/notes/notes"
 import Footer from "./components/footer/footer"
+import NoteController from "./controlers/notes.controllers"
 
 import "./App.scss"
 import sample from "./assets/video.mp4"
+import { IoMdVolumeOff, IoMdVolumeHigh } from "react-icons/io"
+
+const noteController = new NoteController()
 
 const App = () => {
 	// notes, matriz usada para inserção das notas(JSON)
 	const [notes, setNotes] = useState([]);
-
-	// Data e Hora que a nota foi criado
-	let currentTime = new  Date()
-	let currentTimeHours = currentTime.getHours()
-	let currentTimeMinutes = currentTime.getMinutes()
-	let currentTimeDate = currentTime.getDate()
-	let currentTimeMonth = currentTime.getMonth() +1
-	if (currentTimeMonth < 10) {
-		currentTimeMonth = `0${currentTimeMonth}`
-	}
-	let currentTimeYear = currentTime.getFullYear()
-	let timeFormatted = `${currentTimeDate}/${currentTimeMonth}/${currentTimeYear} ás ${currentTimeHours}:${currentTimeMinutes}`;
-	const [time, setTime] = useState(timeFormatted);
-
-	function CreateData(note) {
-		const data = localStorage.getItem('NotePad@notes');
-		const oldData = data ? JSON.parse(data) : [];
-
-		// note recebe id
-		if(notes.length > 0)
-			note.id = (notes[notes.length -1].id +1)
-		else
-			note.id = 0
-		
-		setTime(timeFormatted)
-		note.lastEditionTime = time
-
-		const newData = [
-			...oldData,
-			note,
-		]
-
-		localStorage.setItem('NotePad@notes', JSON.stringify(newData));
-
-		setNotes(newData);
-	}
-	function RemoveData(content) {
-		let storedContent = JSON.parse(localStorage.getItem("NotePad@notes"));
-		storedContent.splice(content.id, 1)
-		setNotes(storedContent);
-		
-		// Atualização dos IDs restantes, apos a remoção
-		let idOperation = storedContent.slice(content.id, storedContent.length +1)
-		for (let i in idOperation) {
-			idOperation[i].id = idOperation[i].id -1;
-		}
-
-		localStorage.setItem('NotePad@notes', JSON.stringify(storedContent));
-	}
+	const [isMuted, setIsMuted] = useState(true)
+	const [isUpNotes, setIsUpNotes] = useState(false)
 
 	useEffect(() => {
-		const data = localStorage.getItem('NotePad@notes');
-		const notes = data ? JSON.parse(data) : [];
+		setNotes(noteController.find())
 
-		setNotes(notes);
-	}, []);
+		setIsUpNotes(false)
+	}, [isUpNotes]);
 
+	function handleCreateNote(newNote) {
+		noteController.create(newNote)
+
+		setIsUpNotes(true)
+	}
+
+	function handleRemoveNote(removeNote) {
+		noteController.remove(removeNote)
+
+		setIsUpNotes(true)
+	}
+	
 	return (
 		<>
 			<div className="container">
+				<button
+					className="muted"
+					onClick={() => setIsMuted(!isMuted)}
+				>
+					{
+						isMuted ? <IoMdVolumeOff /> : <IoMdVolumeHigh  />
+					}
+				</button>
 				<TopBar
-					CreateData={CreateData}
+					CreateData={handleCreateNote}
 				/>
 				<Notes
 					notes={notes}
-					RemoveData={RemoveData}
+					RemoveData={handleRemoveNote}
 				/>
 				<Footer />
-				<video className='myVideo' autoPlay="true" muted loop>
-					<source src={sample} type='video/mp4'/>
+				<video
+					className='myVideo'
+					autoPlay
+					muted={isMuted}
+					loop
+				>
+					<source
+						src={sample}
+						type="video/mp4"
+					/>
 				</video>
 			</div>
 		</>
--- src/components/notes/noteContentPreview.jsx
+++ src/components/notes/noteContentPreview.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React from "react"
 
-import { HiTrash } from "react-icons/hi";
+import { HiTrash } from "react-icons/hi"
+import { format } from "date-fns"
 
 const NoteContentPreview = ({ noteContent, RemoveData }) => {
     let container = document.querySelector("main.notion-container")
     if (noteContent) {
         container.classList.remove("toggerBackground")
-    } 
+    } else {
+        container.classList.add("toggerBackground")
+    }
     const remove = () => {
         RemoveData(noteContent)
-        container.classList.add("toggerBackground")
     }
     return (
         < div
@@ -37,7 +39,7 @@ const NoteContentPreview = ({ noteContent, RemoveData }) => {
                 <div
                     className="lastEditionTimeBox"
                 >
-                    <p className="lastEditionTime">{noteContent.lastEditionTime}</p>
+                    <p className="lastEditionTime">{format(new Date(noteContent.dateEdition), "dd/MM/yyyy 'ás' HH:mm")}</p>
                 </div>
             </div>
         </div>
